cd4d68bef09f0c4a29e0b4510440460832ac1020
Brady Smith <jbradysmith@hotmail.com>
2017-06-14 10:48:58 -0500

    Original Project from CodePlex
    Needs to be modified for SSRS2016

---
+++ SCRUBS/ReadMe.txt
@@ -0,0 +1,8 @@
+CodePlex site for downloads, support and license info:
+http://scrubs.codeplex.com
+
+More info on SQL Scrubs or to upgrade to Premium!
+http://www.summitcloud.com/solutions/scrubs
+
+twitter for alerts:
+http://twitter.com/summitcloud
---
+++ SCRUBS/Scripts/Create Job for SCRUBS.sql
@@ -0,0 +1,69 @@
+USE [msdb]
+GO
+
+/****** Object:  Job [SummitCloud SCRUBS - SSRS Log Feeder]    Script Date: 10/20/2009 13:00:18 ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]]    Script Date: 10/20/2009 13:00:18 ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'SummitCloud SCRUBS - SSRS Log Feeder', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [SSRS Log Update]    Script Date: 10/20/2009 13:00:18 ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'SSRS Log Update', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'SSIS', 
+		@command=N'/FILE "C:\Program Files\SummitCloud\SCRUBS\SSIS\SCRUBS_SSRS_Log_Feed.dtsx" /CONNECTION Destination;"\"Data Source=(local);Initial Catalog=SummitCloud_SCRUBS;Provider=SQLNCLI10.1;Integrated Security=SSPI;Auto Translate=False;\"" /CONNECTION Source;"\"Data Source=(local);Initial Catalog=ReportServer;Provider=SQLNCLI10.1;Integrated Security=SSPI;Auto Translate=False;\"" /CHECKPOINTING OFF /SET "\Package.Variables[User::FullLogging].Properties[Value]";True /REPORTING E', 
+		@database_name=N'master', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Every 5 minutes', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=4, 
+		@freq_subday_interval=5, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20091012, 
+		@active_end_date=99991231, 
+		@active_start_time=0, 
+		@active_end_time=235959, 
+		@schedule_uid=N'5c5a69fe-a59b-4f14-9dd4-e2dee924168e'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+
+GO
+
+
